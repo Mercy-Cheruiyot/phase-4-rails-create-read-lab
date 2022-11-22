f8966dc7647d3d2812ce5ecddfa4568cb745a43e
Mercy <mercycheruiyot2013@gmail.com>
2022-11-22 17:02:27 +0300

    Completed Assignment

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,22 +1,30 @@
-class PlantsController  < ApplicationController
+class PlantsController < ApplicationController
+
+  def index
+      render json: Plant.all
+  end
+
+  def show
+      plants= Plant.find_by(id: (params[:id]))
+      if plants
+          render json:plants
+      else
+          render json:{error:'Plant not found'},status: :not_found
+
+      end
+      
+
+  end
+  def create
+      plants=Plant.create(plant_params)
+      render json: plants ,status: :created
+
+  end
+
+  private
+  def plant_params
+      params.permit(:name, :image, :price)
+  end
 
 
-    def index
-        render json: Plant.all
-    
-      end
-      def show
-        render json: Plant.find_by(id: (params[:id]))
-        
-
-      end
-      def create
-        plants=Plant.create(plant_params)
-        render json: plants ,status: :created
-      end
-    
-      private
-      def plant_params
-        params.permit(:name, :image, :price)
-      end
 end
